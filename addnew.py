4b90d30ad350d2ad33944ffadfe6d5d68675085a
Yichung Hsu <fov223@gmail.com>
2016-12-14 20:18:12 +0800

    create data/set2

--- addnew.py
+++ addnew.py
@@ -3,23 +3,22 @@ import urllib2
 import pandas as pd
 from util import getAlbumDetail, getFilename
 
-data_dir = "data/set1/"
 data_type = {"Filename": str, "Genres": str, "Release Year": int}
 
-def addAlbum(query, dir=data_dir, idx=[0,0]):
-    albums = pd.read_csv(dir+"albumlabel.csv", dtype=data_type, parse_dates=["Release Year"])
+def addAlbum(query, datadir, idx=[0,0]):
+    albums = pd.read_csv(datadir+"albumlabel.csv", dtype=data_type, parse_dates=["Release Year"])
     detail = getAlbumDetail(query, idx=idx)
     if detail['coversize']==False:
         print("unmatched pixel size")
         return None
     filename = getFilename(detail)
     u = urllib2.urlopen(detail['coverurl'])
-    f = open(dir+"img/"+filename+'.jpg', 'wb')
+    f = open(datadir+"img/"+filename+'.jpg', 'wb')
     f.write(u.read())
     f.close()
     newdata = pd.Series({"Filename": filename, "Genres": ' '.join(detail['genres']), "Release Year": detail['release_date']})
     if filename not in albums['Filename'].get_values():
         albums = albums.append(newdata, ignore_index=True)
-        albums.to_csv(data_dir+'albumlabel.csv', index=False)
+        albums.to_csv(datadir+'albumlabel.csv', index=False)
     else:
         print('album already exists')
