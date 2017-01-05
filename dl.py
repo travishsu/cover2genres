f535b3b2bd3ceb10ff9fcb05a7ec4ecb6de56f84
Yichung Hsu <fov223@gmail.com>
2017-01-05 11:14:19 +0800

    commit

--- dl.py
+++ dl.py
@@ -1,16 +1,22 @@
 from numpy import array, zeros, log, exp
 from PIL import Image
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import hamming_loss
 import pandas as pd
 import numpy as np
 
 from keras.preprocessing.text import Tokenizer
 from keras.models import Sequential
+from keras.optimizers import SGD
 from keras.layers import Conv2D, AtrousConvolution2D, Flatten, Dense, MaxPooling2D, Dropout, ZeroPadding2D
 from keras.layers.normalization import BatchNormalization
 from keras import backend as K
 
 data_dir = "data/set2/"
+topN     = 12
+excludeOther = True
+
 data_type = {"Filename": str, "Genres": str, "Release Year": int}
 albums = pd.read_csv(data_dir + "albumlabel.csv", dtype=data_type, parse_dates=["Release Year"])
 
@@ -21,7 +27,7 @@ token.fit_on_texts(genres)
 
 idx_word = dict()
 for key in token.word_index.keys():
-    idx_word[token.word_index[key]] = key
+    idx_word[token.word_index[key]-1] = key
 
 label_lst = albums.Genres.get_values()
 data_y = zeros((len(label_lst), max(token.word_index.values())+1))
@@ -47,13 +53,19 @@ X -= Xmean
 X /= (Xstd+0.0001)
 
 # compute loss weight for each genres cuz training data are unbalanced on genres
-data_y = data_y[:, 2:]
+data_y = data_y[:, 1:]
+new_index = [idx_word[i] for i in xrange(topN)]
+if excludeOther:
+    data_y = data_y[:, :topN]
+else:
+    tmp = np.sum(data_y[:, topN:], axis = 1)
+    tmp = tmp / np.maximum(1, tmp)
+    data_y = np.concatenate( (data_y[:, :topN] , tmp.astype(int).reshape(data_y.shape[0], 1)), axis = 1)
+    new_index.append('other')
 cate_sum = np.sum(data_y, axis=0) # cate_sum is a decreasing sequence
-for i in xrange(len(cate_sum)):
-    if cate_sum[i]<10: # the criteria is to get rid of the genres with less than 10 training data
-        break
-data_y = data_y[:,:i]
 
+label_cardinality = np.mean(cate_sum)
+label_density     = label_cardinality/np.sum(cate_sum)
 # split data
 train_x, test_x, train_y, test_y = train_test_split(X, data_y, test_size=0.2)
 
@@ -102,13 +114,15 @@ model.add(Dense(train_y.shape[1], activation='sigmoid'))
 #    return x / (K.max(x))
 #model.add(Activation(f))
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='adadelta',
+sgd = SGD(lr=0.01, decay=1e-6, momentum=1, nesterov=True)
+
+model.compile(loss='binary_crossentropy',
+              optimizer='sgd',
               metrics=['accuracy'])
 
 model.fit(train_x, train_y,
-          validation_data = (test_x, test_y),
-          batch_size=32,
+          #validation_data = (test_x, test_y),
+          batch_size=256,
           nb_epoch=100,
           shuffle=True,
           class_weight='auto')
@@ -120,7 +134,7 @@ def greater50percent(y):
         tmp = []
         for i in xrange(len(s)):
             if s[i] > 0.5:
-                tmp.append(i)
+                tmp.append(new_index[i])
         p.append(tmp)
     return p
 
@@ -128,8 +142,26 @@ pred = model.predict(test_x)
 p  = greater50percent(pred)
 p_ = greater50percent(test_y)
 
+# Refer to https://github.com/suraj-deshmukh/Multi-Label-Image-Classification/blob/master/miml.ipynb
+threshold = np.arange(0.1,0.9,0.1)
+acc = []
+accuracies = []
+best_threshold = np.zeros(pred.shape[1])
+for i in range(pred.shape[1]):
+    y_prob = np.array(pred[:,i])
+    for j in threshold:
+        y_pred = [1 if prob>=j else 0 for prob in y_prob]
+        acc.append( matthews_corrcoef(test_y[:,i],y_pred))
+    acc   = np.array(acc)
+    index = np.where(acc==acc.max())
+    accuracies.append(acc.max())
+    best_threshold[i] = threshold[index[0][0]]
+    acc = []
+pred_y = np.array([[1 if pred[i,j]>=best_threshold[j] else 0 for j in range(test_y.shape[1])] for i in range(len(test_y))])
+
 for i in xrange(len(p)):
     print(i, p[i], p_[i])
+print(hamming_loss(pred_y, test_y))
 
 def predict_req(filename, model):
     reqdata = array([array(Image.open(filename))])
